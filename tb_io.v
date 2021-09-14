1dfbbfa12859092029642edeba55da03f825b96f
Cristian <crisdaviiid@gmail.com>
2021-09-14 13:14:22 -0300

    tb input output

--- tb_io.v
+++ tb_io.v
@@ -3,9 +3,9 @@
 // Company: 
 // Engineer: 
 // 
-// Create Date: 13.09.2021 20:37:49
+// Create Date: 14.09.2021 12:05:13
 // Design Name: 
-// Module Name: tb_alu
+// Module Name: tb_io
 // Project Name: 
 // Target Devices: 
 // Tool Versions: 
@@ -19,9 +19,8 @@
 // 
 //////////////////////////////////////////////////////////////////////////////////
 
-
-module tb_alu();
-
+module tb_io();
+  
   localparam  NB_IN =8;
   localparam  NB_OUT = 8;
   localparam  NB_CODE =6;
@@ -34,60 +33,98 @@ module tb_alu();
   localparam SRL = 6'b000010;
   localparam NOR = 6'b100111;
   localparam FINISH = 6'b111111;
-
-  reg [NB_CODE - 1 : 0] r_code;
+  
+  
   reg [NB_IN - 1 : 0] r_dato1;
-  reg [NB_IN - 1 : 0] r_dato2; 
+  reg [NB_IN - 1 : 0] r_dato2;
+  reg [NB_IN - 1 : 0] r_switch; 
+  reg b_dato1;
+  reg b_dato2;
+  reg b_code;
   wire [NB_OUT - 1 : 0] r_salida;
   reg clk;
-
-  alu#
-  (.NB_IN(NB_IN), .NB_OUT(NB_OUT), .NB_CODE(NB_CODE))
-  u_alu
-  (.salida(r_salida), .dato1(r_dato1),.dato2(r_dato2), .op_code(r_code));
-
-  initial begin
-    //$dumpfile("dump.vcd"); $dumpvars;
-    r_code = 6'b0;
-    r_dato1 = 8'b0;
-    r_dato2 = 8'b0;
-    clk = 1'b0;
-
-    #20
-    r_dato1= $urandom();
-    r_dato2= $urandom();
-    r_code= ADD;
-    
-    #20
-    r_code= SUB;
-    
-    #20
-    r_code= AND;
-    
-    #20
-    r_code= OR;
-    
-    #20
-    r_code= XOR;
-    
-    #20
-    r_code= SRA;
-    
-    #20
-    r_code= SRL;
-    
-    #20
-    r_code= NOR;
-    
-    #20
-    r_code= FINISH;//Para no imprimir
-    //$finish;
-  end
   
+  input_output#
+  (.NB_IN(NB_IN), .NB_OUT(NB_OUT), .NB_CODE(NB_CODE))
+  u_input_output
+  (.switch(r_switch), .b_dato1(b_dato1), .b_dato2(b_dato2), .b_code(b_code), .clk(clk), .salida(r_salida));
+  
+  initial begin
+    #40
+    r_dato1 = 8'b00010000;//$urandom();
+    r_switch = r_dato1;
+    #20
+    b_dato1 = 8'b1;
+    #20
+    b_dato1 = 8'b0;
+    #20
+    r_dato2 = 8'b00000111;//$urandom();
+    r_switch = r_dato2;
+    #20
+    b_dato2 = 8'b1;
+    #20
+    b_dato2 = 8'b0;
+    #20
+    r_switch = ADD;
+    #20
+    b_code = 8'b1;
+    #20
+    b_code = 8'b0;
+
+    #40
+    r_switch = SUB;
+    #20
+    b_code = 8'b1;
+    #20
+    b_code = 8'b0;
+
+    #40
+    r_switch = AND;
+    #20
+    b_code = 8'b1;
+    #20
+    b_code = 8'b0;
+
+    #40
+    r_switch = OR;
+    #20
+    b_code = 8'b1;
+    #20
+    b_code = 8'b0;
+
+    #40
+    r_switch = XOR;
+    #20
+    b_code = 8'b1;
+    #20
+    b_code = 8'b0;
+
+    #40
+    r_switch = SRA;
+    #20
+    b_code = 8'b1;
+    #20
+    b_code = 8'b0;
+    
+    #40
+    r_switch = SRL;
+    #20
+    b_code = 8'b1;
+    #20
+    b_code = 8'b0;
+
+    #40
+    r_switch = NOR;
+    #20
+    b_code = 8'b1;
+    #20
+    b_code = 8'b0;
+
+  end
   always #10 clk = ~clk;
   
   always@(posedge clk) begin
-    case(r_code)
+    case(r_switch)
       ADD:
         if(r_salida == r_dato1 + r_dato2)
           $display(" ADD CORRECTO ");
